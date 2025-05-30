319432739c6f21798d44da7a9c4a2423b78069be
isaquebarisch <isaquebarisch@gmail.com>
2025-05-30 20:23:17 -0300

    add Maven wrapper, PostgreSQL container configuration, OpenAPI setup, and application structure

---
+++ src/main/java/com/desafio/tecnico/exception/InvalidOperationException.java
@@ -0,0 +1,7 @@
+package com.desafio.tecnico.exception;
+
+public class InvalidOperationException extends RuntimeException {
+    public InvalidOperationException(String message) {
+        super(message);
+    }
+}
