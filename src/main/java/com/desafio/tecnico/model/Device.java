be0f1587bb01cfd606c71a56e5c9fb82ac26cb94
isaquebarisch <isaquebarisch@gmail.com>
2025-05-31 15:31:01 -0300

    refactor Device class to remove Lombok annotations and implement manual constructors, getters, setters, equals, hashCode, and toString methods

--- src/main/java/com/desafio/tecnico/model/Device.java
+++ src/main/java/com/desafio/tecnico/model/Device.java
@@ -1,17 +1,12 @@
 package com.desafio.tecnico.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Entity
 @Table(name = "devices")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Device {
 
     @Id
@@ -34,6 +29,84 @@ public class Device {
     @PrePersist
     public void prePersist() {
         this.creationTime = LocalDateTime.now();
+    }
+
+    // Construtores
+    public Device() {
+    }
+
+    public Device(Long id, String name, String brand, DeviceState state, LocalDateTime creationTime) {
+        this.id = id;
+        this.name = name;
+        this.brand = brand;
+        this.state = state;
+        this.creationTime = creationTime;
+    }
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public DeviceState getState() {
+        return state;
+    }
+
+    public void setState(DeviceState state) {
+        this.state = state;
+    }
+
+    public LocalDateTime getCreationTime() {
+        return creationTime;
+    }
+
+    public void setCreationTime(LocalDateTime creationTime) {
+        this.creationTime = creationTime;
+    }
+
+    // equals, hashCode e toString
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Device device = (Device) o;
+        return Objects.equals(id, device.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Device{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", brand='" + brand + '\'' +
+                ", state=" + state +
+                ", creationTime=" + creationTime +
+                '}';
     }
 
     public enum DeviceState {
