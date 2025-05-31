1b4ccd097504c19ef78a63379d2decc8e653c207
isaquebarisch <isaquebarisch@gmail.com>
2025-05-31 15:31:34 -0300

    refactor OpenApiConfig to simplify API information by removing contact and license details

--- src/main/java/com/desafio/tecnico/config/OpenApiConfig.java
+++ src/main/java/com/desafio/tecnico/config/OpenApiConfig.java
@@ -1,9 +1,7 @@
 package com.desafio.tecnico.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,13 +13,7 @@ public class OpenApiConfig {
         return new OpenAPI()
                 .info(new Info()
                         .title("Devices Management API")
-                        .description("API para gerenciamento de dispositivos com operações CRUD e validações de domínio")
-                        .version("1.0.0")
-                        .contact(new Contact()
-                                .name("Administrador")
-                                .email("admin@example.com"))
-                        .license(new License()
-                                .name("Apache 2.0")
-                                .url("https://www.apache.org/licenses/LICENSE-2.0")));
+                        .description("API para gerenciamento de dispositivos")
+                        .version("1.0.0"));
     }
 }
