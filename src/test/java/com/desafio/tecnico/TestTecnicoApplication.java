461ae2a00f65475c2d58a2ac0562effbe3b09b6c
isaquebarisch <isaquebarisch@gmail.com>
2025-05-31 15:31:50 -0300

    refactor TestTecnicoApplication to integrate PostgreSQL container setup for testing

--- src/test/java/com/desafio/tecnico/TestTecnicoApplication.java
+++ src/test/java/com/desafio/tecnico/TestTecnicoApplication.java
@@ -1,11 +1,23 @@
 package com.desafio.tecnico;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
 
+@TestConfiguration(proxyBeanMethods = false)
 public class TestTecnicoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.from(Main::main).with(TestcontainersConfiguration.class).run(args);
-	}
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer<?> postgresContainer() {
+        return new PostgreSQLContainer<>("postgres:latest");
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.from(Main::main)
+                .with(TestTecnicoApplication.class)
+                .run(args);
+    }
 }
