f315e807dd6040b33f8118ff138e0fe786f650c0
isaquebarisch <isaquebarisch@gmail.com>
2025-05-31 15:31:42 -0300

    refactor TestcontainersConfiguration to enhance PostgreSQL container setup with database name, username, and password

--- src/test/java/com/desafio/tecnico/TestcontainersConfiguration.java
+++ src/test/java/com/desafio/tecnico/TestcontainersConfiguration.java
@@ -4,15 +4,16 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.utility.DockerImageName;
 
-@TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
+@TestConfiguration
+public class TestcontainersConfiguration {
 
-	@Bean
-	@ServiceConnection
-	PostgreSQLContainer<?> postgresContainer() {
-		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
-	}
-
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer<?> postgreSQLContainer() {
+        return new PostgreSQLContainer<>("postgres:latest")
+                .withDatabaseName("devices_test_db")
+                .withUsername("testuser")
+                .withPassword("testpass");
+    }
 }
